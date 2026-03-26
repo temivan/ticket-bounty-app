5c75588fb8d7b9d7cea31e88b55608cb1dbb31cd
temcha <temchurivan@gmail.com>
2026-03-26 19:00:40 +0200

    added tickets sample data links

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,11 +1,23 @@
+import { initialTickets } from "@/data";
+
 type TicketPageProps = {
   params: Promise<{ ticketId: string }>;
 };
 
 const TicketPage = async ({ params }: TicketPageProps) => {
   const { ticketId } = await params;
+  const ticket = initialTickets.find((t) => t.id === ticketId);
 
-  return <h2 className="text-lg">TicketPage: {ticketId}</h2>;
+  if (!ticket) {
+    return <div>Ticket not found</div>;
+  }
+
+  return (
+    <div>
+      <h2 className="text-lg">{ticket.title}</h2>
+      <p className="text-sm">{ticket.content}</p>
+    </div>
+  );
 };
 
 export default TicketPage;
