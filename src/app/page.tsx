976a2791e31b6e2a571aff6f73ac0a714424a352
temcha <temchurivan@gmail.com>
2026-03-26 19:09:45 +0200

    added navigation paths store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+import Link from "next/link";
+import { ticketsPath } from "@/paths";
+
 const HomePage = () => {
-  return <h2 className="text-lg">HomePage</h2>;
+  return (
+    <div>
+      <h2 className="text-lg">HomePage</h2>
+
+      <Link href={ticketsPath()} className="text-sm underline">
+        Go to Tickets
+      </Link>
+    </div>
+  );
 };
 
 export default HomePage;
